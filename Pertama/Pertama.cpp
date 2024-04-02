2b3afc947c7b4025c38919148882127d9d9176ed
vararais <varadigmaisnann@gmail.com>
2024-04-02 19:35:14 +0700

    Menambah Kelengkapan Data

--- Pertama/Pertama.cpp
+++ Pertama/Pertama.cpp
@@ -8,7 +8,27 @@ struct Mahasiswa {
     int umur;
 
 };
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa :";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa :";
+    cin >> mhs.Nama;
+    cout << "Alamat Mahasiswa :";
+    cin >> mhs.Alamat;
+    cout << "Umur Mahasiswa :";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "n\ NIM :" << mhs.NIM;
+    cout << "n\ Nama :" << mhs.Nama;
+    cout << "n\ Alamat :" << mhs.Alamat;
+    cout << "n\ Umur :" << mhs.umur;
+
+
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
