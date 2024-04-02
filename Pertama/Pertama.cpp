9c0bf2a0edab17fc63481f5890d0f996a1be7ceb
vararais <varadigmaisnann@gmail.com>
2024-04-02 19:28:37 +0700

    Menambah Struct Mahasiswa

--- Pertama/Pertama.cpp
+++ Pertama/Pertama.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std;
+
+struct Mahasiswa {
+    string NIM;
+    string Nama;
+    string Alamat;
+    int umur;
+
+};
 int main()
 {
     std::cout << "Hello World!\n";
