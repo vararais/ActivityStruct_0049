d82117aeea7cba08e93e77db120f8ac2e9a0e8db
vararais <varadigmaisnann@gmail.com>
2024-04-02 20:25:02 +0700

    Menambah Struct

--- Keempat/Keempat.cpp
+++ Keempat/Keempat.cpp
@@ -2,6 +2,17 @@
 #include <string>
 using namespace std;
 
+struct DetailAlamat{
+    string desa;
+    string kota;
+}
+
+struct Mahasiswa {
+    string NIM;
+    string Nama;
+    DetailAlamat Alamat;
+    int umur;
+};
 int main()
 {
     std::cout << "Hello World!\n";
