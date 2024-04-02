3aa2aa7d4ceca8344aa4f4cf9b937000c000b78c
vararais <varadigmaisnann@gmail.com>
2024-04-02 20:14:35 +0700

    Menambah Input Output

--- Ketiga/Ketiga.cpp
+++ Ketiga/Ketiga.cpp
@@ -23,4 +23,11 @@ int main()
     getline(cin, mhs.Nama);
 
 
+    cout << "Alamat Mahasiswa :" << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota :";
+    cin >> mhs.alamat.kota;
+    cout << "Umur Mahasiswa :";
+    cin >> mhs.umur;
 }
