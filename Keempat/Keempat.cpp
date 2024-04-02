cfa2c69ff6b852f48aff464db581b34a698fcae2
vararais <varadigmaisnann@gmail.com>
2024-04-02 20:28:14 +0700

    Menambah Int Main

--- Keempat/Keempat.cpp
+++ Keempat/Keempat.cpp
@@ -13,6 +13,38 @@ struct Mahasiswa {
     DetailAlamat Alamat;
     int umur;
 };
+
+int main() {
+	mahasiswa mhs[3];
+	for (int i = 0; i < 3; i++) {
+		cout << "data ke-" << (i + 1) << ":" << endl;
+		cout << "Nomor mahasiswa : ";
+		getline(cin, mhs[i].nim);
+		cout << "Nama Mahasiswa : ";
+		getline(cin, mhs[i].nama);
+
+		cout << "alamat mahasiswa : " << endl;
+		cout << "\t nama Desa : ";
+		cin >> mhs[i].alamat.desa;
+		cout << "\t Nama kota : ";
+		cin >> mhs[i].alamat.kota;
+		cout << "Umur mahasiswa : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
+	}
+	for (int i = 0; i < 3; i++) {
+		cout << endl;
+		cout << "Data mahasiswa ke-" << (i = 1) << ":" << endl;
+		cout << "\n Nim :" << mhs[i].nim;
+		cout << "\n Nama : " << mhs[i].nama;
+		cout << "\n alamat : ";
+		cout << "\n \t Desa : " << mhs[i].alamat.desa;
+		cout << "\n \t Kota : " << mhs[i].alamat.kota;
+		cout << "\n Umur : " << mhs[i].umur;
+		cout << endl;
+	}
+}
 int main()
 {
     std::cout << "Hello World!\n";
